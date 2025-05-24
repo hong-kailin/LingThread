372b1b67223851ee5604a5eea294fddaddf63c14
hong.kailin_win <793831293@qq.com>
2025-05-24 08:02:46 +0800

    feat: 添加被选中单词的高亮功能

--- page/english_edit_widget_page.py
+++ page/english_edit_widget_page.py
@@ -1,6 +1,7 @@
-from PySide6.QtCore import Qt, QPropertyAnimation, Signal, QEasingCurve, Signal, QEvent, QTimer
-from PySide6.QtGui import QCursor
-from PySide6.QtWidgets import QWidget, QMenu
+from PySide6.QtCore import (Qt, QPropertyAnimation, Signal,
+                            QEasingCurve, Signal, QEvent, QTimer)
+from PySide6.QtGui import QCursor, QTextCursor, QTextCharFormat, QColor
+from PySide6.QtWidgets import QWidget, QMenu, QTextEdit
 from ui import Ui_english_edit_widget
 
 
@@ -8,12 +9,57 @@ class EnglishEditWidgetPage(QWidget, Ui_english_edit_widget):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.setupUi(self)
+        self.highlights = []
         self.english_edit.setContextMenuPolicy(Qt.CustomContextMenu)
 
     def show_text_menu(self, pos):
         cursor = self.english_edit.textCursor()
         if cursor.hasSelection():
             menu = QMenu(self)
-            create_action = menu.addAction("创建卡片")
-            # create_action.triggered.connect(lambda: self.create_card(cursor))
-            menu.exec_(self.english_edit.mapToGlobal(pos))
+            menu.setStyleSheet("""
+                QMenu::item {
+                    color: black;
+                    padding: 6px 24px;
+                    background-color: transparent;
+                }
+
+                QMenu::item:selected {
+                    background-color: #e0e7ff;
+                }
+
+                QMenu::separator {
+                    height: 1px;
+                    background: #eee;
+                    margin: 4px 8px;
+                }
+            """)
+            create_action = menu.addAction("create card")
+            create_action.triggered.connect(lambda: self.create_card(cursor))
+            menu.exec(self.english_edit.mapToGlobal(pos))
+
+    def create_card(self, cursor):
+        selected_text = cursor.selectedText().strip()
+        print(selected_text)
+        if not selected_text:
+            return
+        # create card
+        # ====
+        highlight = self.add_highlight(cursor)
+
+    def add_highlight(self, cursor):
+        new_cursor = self.english_edit.textCursor()
+        new_cursor.setPosition(cursor.selectionStart())
+        new_cursor.setPosition(cursor.selectionEnd(), QTextCursor.KeepAnchor)
+
+        format = QTextCharFormat()
+        format.setBackground(QColor(255, 245, 157))
+        format.setUnderlineStyle(QTextCharFormat.SingleUnderline)
+        format.setUnderlineColor(QColor(255, 179, 0))
+
+        extra_selection = QTextEdit.ExtraSelection()
+        extra_selection.format = format
+        extra_selection.cursor = new_cursor
+
+        self.highlights.append(extra_selection)
+        self.english_edit.setExtraSelections(self.highlights)
+        return extra_selection
